9f804bc82c6be614d6d8ab3ff3becb73f86af174
Rupesh <rupesh.csv@gmail.com>
2022-02-22 23:14:19 -0500

    sample model

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
+
+file1 = open('models/model_lr.pkl', 'rb')
+model1 = pickle.load(file1)
+file1.close()
 
 
 @app.route('/', methods=['GET'])
@@ -10,9 +15,11 @@ def home():
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
+    text = 'error'
     if request.method == 'POST':
         text = request.form['text']
-    return render_template('home.html', result_text=f'{text}')
+    prediction = model1.predict([text])[0]
+    return render_template('home.html', result_text=f'{prediction}')
 
 
 if __name__ == '__main__':
